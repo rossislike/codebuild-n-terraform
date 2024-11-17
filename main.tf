fd6ddcb91b894259f81a053dc240b71c462b29d1
rossislike <ross.agginie@gmail.com>
2024-11-17 17:21:39 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.76.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "mybucket" {
+    bucket = "rumo-codebuild-bucket"
+}
